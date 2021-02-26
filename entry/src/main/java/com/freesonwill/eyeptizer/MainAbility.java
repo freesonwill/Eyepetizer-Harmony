47fea5ec216dd24865c40f8e3e377c3cccc13c7c
Zyaire <Zyaire@clenovo.com>
2021-02-26 16:44:40 +0800

    init

---
+++ entry/src/main/java/com/freesonwill/eyeptizer/MainAbility.java
@@ -0,0 +1,16 @@
+package com.freesonwill.eyeptizer;
+
+import com.freesonwill.eyeptizer.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+import ohos.aafwk.content.Operation;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+
+    }
+
+}
